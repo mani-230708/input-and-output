ac908e4a00242262d2af8b939f5347b5346ec433
mani-230708 <22hr1a3262@mtieat.org>
2024-09-19 14:11:22 +0530

    play.py

--- play.py
+++ play.py
@@ -29,3 +29,32 @@ Name:  Kendriya School
 Score: 110
 Overs played: 18
 '''
+
+def get_match_details():
+   
+    print("Team 1:")
+    team1_name = input("Team Name:\n")
+    team1_score = input("Score:\n")
+    team1_overs = input("Overs played:\n")
+
+
+    print("Team 2:")
+    team2_name = input("Team name:\n")
+    team2_score = input("Score:\n")
+    team2_overs = input("Overs played:\n")
+
+
+    print("\nMatch Details:")
+    print("Team 1:")
+    print(f"Name: {team1_name}")
+    print(f"Score: {team1_score}")
+    print(f"Overs played: {team1_overs}")
+    print("Team 2:")
+    print(f"Name: {team2_name}")
+    print(f"Score: {team2_score}")
+    print(f"Overs played: {team2_overs}")
+
+
+get_match_details()
+
+
